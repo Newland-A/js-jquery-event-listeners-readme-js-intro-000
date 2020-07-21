8e91c966c528faf18951bb1ab849677aae1c56c2
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-21 16:27:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function submitIt(){
-  $("form").on('submit', function() { 
+  $('form').on('submit', function() { 
     alert('Your form is going to be submitted now.');
   
 });
