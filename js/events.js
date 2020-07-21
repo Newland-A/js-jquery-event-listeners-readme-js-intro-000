1612663f1b6a5ca60272fd1a78a7e447af858a99
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-21 16:18:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,14 @@ function frameIt() {
 }
 
 function submitIt(){
-  
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 }
 
 function pressIt() {
